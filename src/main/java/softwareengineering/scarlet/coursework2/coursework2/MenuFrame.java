d7150b2db5da93750b26defd949b99227e68851a
Gordon <grr28@bath.ac.uk>
2017-11-17 22:35:21 +0000

    changed menu game title to ASCII art

--- src/main/java/softwareengineering/scarlet/coursework2/coursework2/MenuFrame.java
+++ src/main/java/softwareengineering/scarlet/coursework2/coursework2/MenuFrame.java
@@ -7,15 +7,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Font;
 
-/**
- *
- * @author george
- */
+
 public class MenuFrame extends JFrame {
 
     public MenuFrame() {
 
         initUI();
+        
     }
 
     public void initUI() {
@@ -40,38 +38,62 @@ public class MenuFrame extends JFrame {
             }
 
         });
-
     }
-
 }
 
 class MenuPanel extends JPanel {
-
-    private void doDrawing(Graphics g) {
-
-        Graphics2D g2d = (Graphics2D) g;
-        
-        Font titleFont = new Font("Monospaced", Font.PLAIN, 24);
-        g2d.setFont(titleFont);
-        
-        g2d.drawString("GAME TITLE", 180, 180);
-        
+  
+    private void drawOptions(Graphics2D g2d) {
+       
         Font menuFont = new Font("Monospaced", Font.PLAIN, 12);
         g2d.setFont(menuFont);
         
         g2d.drawString("→ New Game", 180, 250);
         g2d.drawString("  View Leaderboard", 180, 270);
         g2d.drawString("  Quit", 180, 290);
-       
 
+    }
+    
+    void drawTitle(Graphics2D g2d) {
+      String titleArt = 
+          "  ______                                                  ______                                   __     \n" + 
+          " /      \\                                                /      \\                                 /  |    \n" + 
+          "/$$$$$$  | __    __   ______    ______    ______        /$$$$$$  | __    __   ______    _______  _$$ |_   \n" + 
+          "$$ \\__$$/ /  |  /  | /      \\  /      \\  /      \\       $$ |  $$ |/  |  /  | /      \\  /       |/ $$   |  \n" + 
+          "$$      \\ $$ |  $$ |/$$$$$$  |/$$$$$$  |/$$$$$$  |      $$ |  $$ |$$ |  $$ |/$$$$$$  |/$$$$$$$/ $$$$$$/   \n" + 
+          " $$$$$$  |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/       $$ |_ $$ |$$ |  $$ |$$    $$ |$$      \\   $$ | __ \n" + 
+          "/  \\__$$ |$$ \\__$$ |$$ |__$$ |$$$$$$$$/ $$ |            $$ / \\$$ |$$ \\__$$ |$$$$$$$$/  $$$$$$  |  $$ |/  |\n" + 
+          "$$    $$/ $$    $$/ $$    $$/ $$       |$$ |            $$ $$ $$< $$    $$/ $$       |/     $$/   $$  $$/ \n" + 
+          " $$$$$$/   $$$$$$/  $$$$$$$/   $$$$$$$/ $$/              $$$$$$  | $$$$$$/   $$$$$$$/ $$$$$$$/     $$$$/  \n" + 
+          "                    $$ |                                     $$$/                                         \n" + 
+          "                    $$ |                                                                                  \n" + 
+          "                    $$/                                                                                   ";
+      
+      
+      Font titleFont = new Font("Monospaced", Font.BOLD, 6);
+      g2d.setFont(titleFont);
+      
+      // set title draw coordinates
+      int x = 50;
+      int y = 100;
+      
+      // print line by line; N.B. Graphics.drawString method does not break
+      //    across lines, so we must do it for it.
+      for (String line : titleArt.split("\n")) {
+          g2d.drawString(line, x, y += g2d.getFontMetrics().getHeight());
+      }
     }
 
     @Override
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
-        doDrawing(g);
+        
+        Graphics2D g2d = (Graphics2D) g;
 
+        drawOptions(g2d);
+        drawTitle(g2d);
+        
     }
 
 }
