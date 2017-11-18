ad154341d2ecac9b7d00cccee2541f54ecadb0b3
Daniel Cosser <danbjorn@gmail.com>
2017-11-18 18:42:16 +0000

    Fix up whitespace etc of MenuFrame.java

--- src/main/java/softwareengineering/scarlet/coursework2/MenuFrame.java
+++ src/main/java/softwareengineering/scarlet/coursework2/MenuFrame.java
@@ -10,90 +10,77 @@ import java.awt.Font;
 
 public class MenuFrame extends JFrame {
 
-    public MenuFrame() {
+  public MenuFrame() {
+    initUI();
+  }
 
-        initUI();
-        
-    }
+  public void initUI() {
+    this.add(new MenuPanel());
 
-    public void initUI() {
+    setSize(500, 500);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
-        this.add(new MenuPanel());
-
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
-    public static void main(String[] args) {
-
-        EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-
-                MenuFrame menu = new MenuFrame();
-                menu.setVisible(true);
-
-            }
-
-        });
-    }
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        MenuFrame menu = new MenuFrame();
+        menu.setVisible(true);
+      }
+    });
+  }
 }
 
+
 class MenuPanel extends JPanel {
-  
-    private void drawOptions(Graphics2D g2d) {
-       
-        Font menuFont = new Font("Monospaced", Font.PLAIN, 12);
-        g2d.setFont(menuFont);
-        
-        g2d.drawString("→ New Game", 180, 250);
-        g2d.drawString("  View Leaderboard", 180, 270);
-        g2d.drawString("  Quit", 180, 290);
 
+  private void drawOptions(Graphics2D g2d) {
+    Font menuFont = new Font("Monospaced", Font.PLAIN, 12);
+
+    g2d.setFont(menuFont);
+    g2d.drawString("→ New Game", 180, 250);
+    g2d.drawString("  View Leaderboard", 180, 270);
+    g2d.drawString("  Quit", 180, 290);
+
+  }
+
+  void drawTitle(Graphics2D g2d) {
+    String titleArt =
+        "  ______                                                  ______                                   __     \n"
+            + " /      \\                                                /      \\                                 /  |    \n"
+            + "/$$$$$$  | __    __   ______    ______    ______        /$$$$$$  | __    __   ______    _______  _$$ |_   \n"
+            + "$$ \\__$$/ /  |  /  | /      \\  /      \\  /      \\       $$ |  $$ |/  |  /  | /      \\  /       |/ $$   |  \n"
+            + "$$      \\ $$ |  $$ |/$$$$$$  |/$$$$$$  |/$$$$$$  |      $$ |  $$ |$$ |  $$ |/$$$$$$  |/$$$$$$$/ $$$$$$/   \n"
+            + " $$$$$$  |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/       $$ |_ $$ |$$ |  $$ |$$    $$ |$$      \\   $$ | __ \n"
+            + "/  \\__$$ |$$ \\__$$ |$$ |__$$ |$$$$$$$$/ $$ |            $$ / \\$$ |$$ \\__$$ |$$$$$$$$/  $$$$$$  |  $$ |/  |\n"
+            + "$$    $$/ $$    $$/ $$    $$/ $$       |$$ |            $$ $$ $$< $$    $$/ $$       |/     $$/   $$  $$/ \n"
+            + " $$$$$$/   $$$$$$/  $$$$$$$/   $$$$$$$/ $$/              $$$$$$  | $$$$$$/   $$$$$$$/ $$$$$$$/     $$$$/  \n"
+            + "                    $$ |                                     $$$/                                         \n"
+            + "                    $$ |                                                                                  \n"
+            + "                    $$/                                                                                   ";
+
+    Font titleFont = new Font("Monospaced", Font.BOLD, 6);
+    g2d.setFont(titleFont);
+
+    // Set title draw coordinates
+    int x = 50;
+    int y = 100;
+
+    // Print line by line; N.B. Graphics.drawString method does not break
+    // across lines, so we must do it for it.
+    for (String line : titleArt.split("\n")) {
+      g2d.drawString(line, x, y += g2d.getFontMetrics().getHeight());
     }
-    
-    void drawTitle(Graphics2D g2d) {
-      String titleArt = 
-          "  ______                                                  ______                                   __     \n" + 
-          " /      \\                                                /      \\                                 /  |    \n" + 
-          "/$$$$$$  | __    __   ______    ______    ______        /$$$$$$  | __    __   ______    _______  _$$ |_   \n" + 
-          "$$ \\__$$/ /  |  /  | /      \\  /      \\  /      \\       $$ |  $$ |/  |  /  | /      \\  /       |/ $$   |  \n" + 
-          "$$      \\ $$ |  $$ |/$$$$$$  |/$$$$$$  |/$$$$$$  |      $$ |  $$ |$$ |  $$ |/$$$$$$  |/$$$$$$$/ $$$$$$/   \n" + 
-          " $$$$$$  |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/       $$ |_ $$ |$$ |  $$ |$$    $$ |$$      \\   $$ | __ \n" + 
-          "/  \\__$$ |$$ \\__$$ |$$ |__$$ |$$$$$$$$/ $$ |            $$ / \\$$ |$$ \\__$$ |$$$$$$$$/  $$$$$$  |  $$ |/  |\n" + 
-          "$$    $$/ $$    $$/ $$    $$/ $$       |$$ |            $$ $$ $$< $$    $$/ $$       |/     $$/   $$  $$/ \n" + 
-          " $$$$$$/   $$$$$$/  $$$$$$$/   $$$$$$$/ $$/              $$$$$$  | $$$$$$/   $$$$$$$/ $$$$$$$/     $$$$/  \n" + 
-          "                    $$ |                                     $$$/                                         \n" + 
-          "                    $$ |                                                                                  \n" + 
-          "                    $$/                                                                                   ";
-      
-      
-      Font titleFont = new Font("Monospaced", Font.BOLD, 6);
-      g2d.setFont(titleFont);
-      
-      // set title draw coordinates
-      int x = 50;
-      int y = 100;
-      
-      // print line by line; N.B. Graphics.drawString method does not break
-      //    across lines, so we must do it for it.
-      for (String line : titleArt.split("\n")) {
-          g2d.drawString(line, x, y += g2d.getFontMetrics().getHeight());
-      }
-    }
+  }
 
-    @Override
-    public void paintComponent(Graphics g) {
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g;
 
-        super.paintComponent(g);
-        
-        Graphics2D g2d = (Graphics2D) g;
+    drawOptions(g2d);
+    drawTitle(g2d);
 
-        drawOptions(g2d);
-        drawTitle(g2d);
-        
-    }
-
+  }
 }
