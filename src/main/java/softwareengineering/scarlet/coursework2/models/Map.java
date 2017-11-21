006caefb02929a0e272fc45bdb1b4e7b3444a36b
Daniel Cosser <danbjorn@gmail.com>
2017-11-21 08:39:38 +0000

    Fill in some fields of the map class

--- src/main/java/softwareengineering/scarlet/coursework2/models/Map.java
+++ src/main/java/softwareengineering/scarlet/coursework2/models/Map.java
@@ -1,5 +1,39 @@
 package softwareengineering.scarlet.coursework2.models;
 
-public class Map {
+import java.util.ArrayList;
 
+public class Map {
+  private ArrayList<Room> rooms;
+  private ArrayList<Corridor> corridors;
+  private int[][] cache;
+  private int width;
+  private int height;
+
+  public Map(int width, int height) {
+    this.rooms = new ArrayList<Room>();
+    this.corridors = new ArrayList<Corridor>();
+    this.width = width;
+    this.height = height;
+    this.cache = new int[width][height];
+  }
+
+  public ArrayList<Room> getRooms() {
+    return rooms;
+  }
+
+  public ArrayList<Corridor> getCorridors() {
+    return corridors;
+  }
+
+  public int[][] getCache() {
+    return cache;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
 }
