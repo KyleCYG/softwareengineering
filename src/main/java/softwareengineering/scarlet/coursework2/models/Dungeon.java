9cc33950d5b3046616204fa3886816d080e201b7
grr28 <grr28@CSPC-LAB12.campus.bath.ac.uk>
2017-11-27 12:45:21 +0000

    Made Dungeon generate goldItems and populate Level on level generation

--- src/main/java/softwareengineering/scarlet/coursework2/models/Dungeon.java
+++ src/main/java/softwareengineering/scarlet/coursework2/models/Dungeon.java
@@ -21,12 +21,32 @@ public class Dungeon {
     List<Level> levels = new ArrayList<Level>();
 
     for (int i = 0; i < numLevels; i++) {
-      levels.add(LevelFactory.generateLevel(width, height, new ArrayList<Entity>()));
+      List<Entity> entities = Dungeon.generateEntities(i); // difficulty is proportional to level
+      levels.add(LevelFactory.generateLevel(width, height, entities));
     }
 
     return levels;
   }
 
+  /**
+   * Generates a list of entities to populate a level with
+   * 
+   * @param difficulty An arbitrary number representing difficulty, higher is harder
+   * @return A list of entities
+   */
+  protected static List<Entity> generateEntities(int difficulty) {
+    List<Entity> entities = new ArrayList<Entity>();
+    
+    int numGoldItems = 4;
+    
+    // Generate gold (with value proportional to difficulty) and add
+    for (int i = 0; i < numGoldItems; i++){
+      entities.add(new GoldItem((difficulty+1) * 10));
+    }
+    
+    return entities;
+  }
+  
   public Level getCurrentLevel() {
     if (this.levels == null) {
       this.levels = Dungeon.generateLevels(this.width, this.height, this.numLevels);
