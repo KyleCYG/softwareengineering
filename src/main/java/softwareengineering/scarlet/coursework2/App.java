730a16766c113a171b09aa386cffee91cc8cb12e
Daniel Cosser <danbjorn@gmail.com>
2017-11-29 08:55:17 +0000

    Add some docs to the App class
    Also fixed some formatting / imports

--- src/main/java/softwareengineering/scarlet/coursework2/App.java
+++ src/main/java/softwareengineering/scarlet/coursework2/App.java
@@ -1,12 +1,28 @@
 package softwareengineering.scarlet.coursework2;
 
 import java.awt.EventQueue;
-import softwareengineering.scarlet.coursework2.controllers.Controller;
 import softwareengineering.scarlet.coursework2.controllers.MenuController;
 import softwareengineering.scarlet.coursework2.views.Frame;
 import softwareengineering.scarlet.coursework2.views.MenuView;
 import softwareengineering.scarlet.coursework2.views.Panel;
 
+/**
+ * Main entry point into the application.
+ *
+ * Contains the main method, kicking off AWT etc. Also acts as a central point of reference for all
+ * controllers, allowing them to switch without having to store references to each other.
+ *
+ * Each controller will have a reference to the App, and should use it to pass control to other
+ * controllers/views via the switchTo* methods.
+ *
+ * There should be a switchTo* method for each controller, which should act as a factory method for
+ * the controller. This should also include the creation of the relevant View, so that the view
+ * implementation is separated from the controller. Other objects should be instantiated by the
+ * controller itself.
+ *
+ * @author Gordon Rennie
+ * @author Dan Cosser
+ */
 public class App implements GameApp {
   private Frame frame;
   private Panel panel;
@@ -16,25 +32,39 @@ public class App implements GameApp {
     this.panel = new Panel();
     this.frame = new Frame(this.panel);
   }
+
+  /**
+   * Switch control to the Menu
+   */
   public void switchToMenu() {
     if (this.menuController == null) {
       this.menuController = new MenuController(this);
     }
 
     this.panel.setController(this.menuController);
-    
+
     MenuView view = new MenuView();
     this.menuController.init(view);
   }
 
+  /**
+   * Quit the application
+   */
   public void quit() {
     System.exit(0);
   }
 
-  public static void main(String[] args) { 
+  /**
+   * Main method.
+   *
+   * Creates the App (including basic AWT resources), and then surrenders control to the
+   * MenuController.
+   *
+   * @param args Command line args - none respected by this application.
+   */
+  public static void main(String[] args) {
     EventQueue.invokeLater(new Runnable() {
       public void run() {
-        
         // Create the app
         App app = new App();
 
