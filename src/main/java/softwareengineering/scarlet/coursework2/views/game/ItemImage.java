05bde1f1fbeb9bb2d153ed1350990b0a82938852
Daniel Cosser <danbjorn@gmail.com>
2017-12-04 15:24:11 +0000

    Fix an error message that didn't actually report any information

--- src/main/java/softwareengineering/scarlet/coursework2/views/game/ItemImage.java
+++ src/main/java/softwareengineering/scarlet/coursework2/views/game/ItemImage.java
@@ -29,7 +29,7 @@ public class ItemImage {
         ImageIcon icon = new ImageIcon(ImageIO.read(stream));
         this.image = icon.getImage();
       } catch (IOException io) {
-        throw new RuntimeException("Image %s not found! Assuming problem with resources and quitting");
+        throw new RuntimeException(String.format("Image %s not found! Assuming problem with resources and quitting", this.filename));
       }
     }
     return this.image;
