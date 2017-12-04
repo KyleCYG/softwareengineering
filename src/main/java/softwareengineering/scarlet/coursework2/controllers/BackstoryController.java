3baa8ebb27b5ae07dc378152b9baa3bed009d33c
Daniel Cosser <danbjorn@gmail.com>
2017-12-04 13:41:48 +0000

    Tidy up formatting of the backstory controller

--- src/main/java/softwareengineering/scarlet/coursework2/controllers/BackstoryController.java
+++ src/main/java/softwareengineering/scarlet/coursework2/controllers/BackstoryController.java
@@ -7,13 +7,11 @@ import softwareengineering.scarlet.coursework2.views.View;
 public class BackstoryController implements Controller{
   private App app;
   private BackstoryView view;
-  
+
   public BackstoryController(App app) {
-    
     this.app = app;
-    
   }
-  
+
   /**
    * Switch to the back story screen.
    *
@@ -22,29 +20,22 @@ public class BackstoryController implements Controller{
   @Override
   public void init(View view) {
     this.view = (BackstoryView) view;
-    
   }
 
   @Override
   public View getView() {
-    // TODO Auto-generated method stub
     return this.view;
   }
 
   @Override
   /**
-   * user should press enter to continue to the game
+   * User should press enter to continue to the game
+   *
    * @param input
    */
   public void handleInput(Input input) {
-    // TODO Auto-generated method stub
-    switch(input) {
-      case CHOOSE:
+    if (input == Input.CHOOSE) {
         app.switchToGame();
-        break;
-      
     }
-    
   }
-  
 }
