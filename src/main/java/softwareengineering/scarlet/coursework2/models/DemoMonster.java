b2e185f4497a80bc1caacaf385e33285424244e6
Daniel Cosser <danbjorn@gmail.com>
2017-12-09 23:00:04 +0000

    Started putting together bits for generating monsters
    The Factory will need updating to pick some actual monsters, the DemoMonster is just a placeholder

---
+++ src/main/java/softwareengineering/scarlet/coursework2/models/DemoMonster.java
@@ -0,0 +1,9 @@
+package softwareengineering.scarlet.coursework2.models;
+
+public class DemoMonster extends Monster {
+
+  public DemoMonster(int startX, int startY) {
+    super("Demo Monster", startX, startY, 1, 1);
+  }
+
+}
