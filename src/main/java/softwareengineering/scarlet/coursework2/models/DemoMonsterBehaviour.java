9803721cd37d09cc1fef83e60bdf9b8f253a5ea9
Daniel Cosser <danbjorn@gmail.com>
2017-12-10 09:05:15 +0000

    Fix NPE when moving around, caused by the DemoMonster not having any behaviour

---
+++ src/main/java/softwareengineering/scarlet/coursework2/models/DemoMonsterBehaviour.java
@@ -0,0 +1,10 @@
+package softwareengineering.scarlet.coursework2.models;
+
+public class DemoMonsterBehaviour implements MonsterBehaviour {
+
+  @Override
+  public void performAction(Monster monster, Dungeon dungeon, Player player) {
+    // No-op for demo
+  }
+
+}
