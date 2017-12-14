b91fa318a16aeb251bf18a97e518dbc6087ed7b4
Daniel Cosser <dpc41@bath.ac.uk>
2017-12-14 08:54:01 +0000

    Add a note about future extension of the pdf script

--- src/main/bash/codeaspdf.sh
+++ src/main/bash/codeaspdf.sh
@@ -7,3 +7,5 @@ find ../../../src/main/java -iname "*.java" -type f -exec ./makepdf.sh {} $TMP \
 find ../../../src/test/java -iname "*.java" -type f -exec ./makepdf.sh {} $TMP \;
 pdfjoin $TMP/*.pdf --pagecommand {} --outfile $OUTPUT/code.pdf
 rm -rf $TMP
+
+# \setcounter{page}{1} may work to reset the page numbers if we want to merge into the main report
