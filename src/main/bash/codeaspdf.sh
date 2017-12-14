f276486f9d4cc718f504e12fe5b67a7ac2007fdd
Daniel Cosser <dpc41@bath.ac.uk>
2017-12-14 08:42:20 +0000

    Remove the broken page numbers in the header, and add a fixed one in the footer

--- src/main/bash/codeaspdf.sh
+++ src/main/bash/codeaspdf.sh
@@ -5,5 +5,5 @@ mkdir -p $OUTPUT
 mkdir $TMP
 find ../../../src/main/java -iname "*.java" -type f -exec ./makepdf.sh {} $TMP \;
 find ../../../src/test/java -iname "*.java" -type f -exec ./makepdf.sh {} $TMP \;
-pdfjoin $TMP/*.pdf --outfile $OUTPUT/code.pdf
+pdfjoin $TMP/*.pdf --pagecommand {} --outfile $OUTPUT/code.pdf
 rm -rf $TMP
--- src/main/bash/makepdf.sh
+++ src/main/bash/makepdf.sh
@@ -1,6 +1,6 @@
 set -eu
 FILENAME=${1##*/}
 OUTPUT=$2
-vim $1 -c "hardcopy > $OUTPUT/$FILENAME.ps | q"
+vim $1 "+set printheader=%<%t" -c "hardcopy > $OUTPUT/$FILENAME.ps | q"
 ps2pdf $OUTPUT/$FILENAME.ps $OUTPUT/$FILENAME.pdf
 rm $OUTPUT/$FILENAME.ps
