c70859a5650c5a598498c67566e240584fee10f6
Daniel Cosser <danbjorn@gmail.com>
2017-12-15 13:46:16 +0000

    Remove unused DemoMonsterBehaviour class

--- src/main/java/softwareengineering/scarlet/coursework2/models/DemoMonsterBehaviour.java
+++
@@ -1,9 +0,0 @@
-package softwareengineering.scarlet.coursework2.models;
-
-public class DemoMonsterBehaviour implements MonsterBehaviour {
-
-  @Override
-  public void performAction(Monster monster, Dungeon dungeon, Player player) {
-    // No-op for demo
-  }
-}
