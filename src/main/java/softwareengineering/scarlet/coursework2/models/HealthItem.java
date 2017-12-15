3702fe6b7989bc070221c8e10da6a84a935f880f
Daniel Cosser <danbjorn@gmail.com>
2017-12-15 12:46:18 +0000

    Remove value from health and gold
    Unused feature!

--- src/main/java/softwareengineering/scarlet/coursework2/models/HealthItem.java
+++ src/main/java/softwareengineering/scarlet/coursework2/models/HealthItem.java
@@ -4,14 +4,7 @@ package softwareengineering.scarlet.coursework2.models;
  * A health item on the level, collected by the player to regain health
  */
 public class HealthItem extends Entity {
-  private int value;
-
-  public HealthItem(int value) {
-    this.value = value;
+  public HealthItem() {
     this.type = CellType.HEALTH;
-  }
-
-  public int getValue() {
-    return value;
   }
 }
